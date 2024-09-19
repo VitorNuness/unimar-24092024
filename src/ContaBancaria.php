b3aa6ff94ec33cb6e5d5f8d6340a444b26a82254
Maycon Macedo <darkyuse@gmail.com>
2024-09-19 15:50:29 -0300

    Adicionando o construct

--- src/ContaBancaria.php
+++ src/ContaBancaria.php
@@ -4,5 +4,14 @@ namespace Src;
 
 class ContaBancaria
 {
-    //
+    private $saldo;
+    private $cliente;
+    private $historicoTransacoes;
+
+    public function __construct($cliente, $saldoInicial = 0)
+    {
+        $this->cliente = $cliente;
+        $this->saldo = $saldoInicial;
+        $this->historicoTransacoes = [];
+    }
 }
